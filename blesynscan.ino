ed8001c7af2fdbcdb0ee813572cc966f466ec0a4
Emard <vordah@gmail.com>
2024-11-09 15:04:46 +0100

    some comments

--- blesynscan.ino
+++ blesynscan.ino
@@ -3,7 +3,11 @@
     Based on Neil Kolban example for IDF: https://github.com/nkolban/esp32-snippets/blob/master/cpp_utils/tests/BLE%20Tests/SampleNotify.cpp
     Ported to Arduino ESP32 by Evandro Copercini
 
-   board manager esp32 by espressif
+   Install:
+   Board manager -> esp32 by espressif
+
+   Select board:
+   Board -> esp32 -> ESP32 Dev Module
 
    Create a BLE server that, once we receive a connection, will send periodic notifications.
    The service advertises itself as: 6E400001-B5A3-F393-E0A9-E50E24DCCA9E
