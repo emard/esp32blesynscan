cb41da22e3147e1f8dfda7ffac580f31793dc56c
Emard <vordah@gmail.com>
2025-01-15 00:38:32 +0100

    cleanup

--- micropython/synscan_ble.py
+++ micropython/synscan_ble.py
@@ -16,16 +16,16 @@ class BLE():
 
     def __init__(self, name):
         dupterm(None,0) # detach micropython console from tx/rx uart
+        self.ledpin = Pin(LED_PIN, Pin.OUT)
         self.name = name
+        self.uart = UART(1,baudrate=9600,tx=43,rx=44,timeout=10)
+        self.motorfw = None
         self.ble = bluetooth.BLE()
         self.ble.active(True)
-        self.ledpin = Pin(LED_PIN, Pin.OUT)
         self.disconnected()
         self.ble.irq(self.ble_irq)
         self.register()
         self.advertiser()
-        self.uart = UART(1,baudrate=9600,tx=43,rx=44,timeout=10)
-        self.motorfw = None
 
     def led(self,val):
         self.ledpin.value(val^1)
@@ -66,25 +66,25 @@ class BLE():
 
     def register(self):
         # Nordic UART Service (NUS)
-        #NUS_UUID = '6E400001-B5A3-F393-E0A9-E50E24DCCA9E'
+        #SERVICE_UUID = '6E400001-B5A3-F393-E0A9-E50E24DCCA9E'
         #RX_UUID  = '6E400002-B5A3-F393-E0A9-E50E24DCCA9E'
         #TX_UUID  = '6E400003-B5A3-F393-E0A9-E50E24DCCA9E'
 
         # synscan_ble serial
-        NUS_UUID = '0000a002-0000-1000-8000-00805F9B34FB'
+        SERVICE_UUID = '0000a002-0000-1000-8000-00805F9B34FB'
         RX_UUID  = '0000c302-0000-1000-8000-00805F9B34FB'
         TX_UUID  = '0000c306-0000-1000-8000-00805F9B34FB'
 
-        BLE_NUS = bluetooth.UUID(NUS_UUID)
+        BLE_SERVICE = bluetooth.UUID(SERVICE_UUID)
         BLE_RX = (bluetooth.UUID(RX_UUID), bluetooth.FLAG_WRITE)
         BLE_TX = (bluetooth.UUID(TX_UUID), bluetooth.FLAG_NOTIFY)
 
-        BLE_UART = (BLE_NUS, (BLE_TX, BLE_RX,))
+        BLE_UART = (BLE_SERVICE, (BLE_TX, BLE_RX,))
         SERVICES = (BLE_UART, )
         ((self.tx, self.rx,), ) = self.ble.gatts_register_services(SERVICES)
 
-    def send(self, data):
-        self.ble.gatts_notify(0, self.tx, data + '\n')
+    #def send(self, data):
+    #    self.ble.gatts_notify(0, self.tx, data)
 
     def advertiser(self):
         name = bytearray(self.name, "UTF-8")
