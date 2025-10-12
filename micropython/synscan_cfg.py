437c741fcd91acd69ca2f09c569b4dbc256f51d8
Emard <vordah@gmail.com>
2025-10-12 13:10:14 +0200

    slow down manual slew

--- micropython/synscan_cfg.py
+++ micropython/synscan_cfg.py
@@ -28,18 +28,29 @@ REPLACE_COMMAND_MINI={
 # at WiFi connect, replace AT commands with b""
 b"AT+CWMODE_CUR?\r\n": b"",
 b"AT+GMR\r\n": b"",
+
 # force main encoders
 #b":W1040000\r": b":W1050000\r", # rewrite AZ  auxiliary as AZ  main encoder
 #b":W2040000\r": b":W2050000\r", # rewrite ALT auxiliary as ALT main encoder
 # force auxiliary encoders
 #b":W1050000\r": b":W1040000\r", # rewrite AZ  main as AZ  auxiliary encoder
 #b":W2050000\r"; b":W2040000\r", # rewrite ALT main as ALT auxiliary encoder
+
 # slow to allow correct counting, 5V operation and quiet
-# MANUAL SLEW SPEED: instead slew 9 use slew 8.5
+
+# MANUAL SLEW SPEED (larger hex number -> slower)
 # caution: encoders loose counts with slews faster than 7
-b":I1500000\r": b":I1600000\r",
-b":I2500000\r": b":I2600000\r",
-# GOTO SLEW SPEED
+# 7: b':I19F0000\r'
+# 8: b':I16A0000\r'
+# 9: b':I1500000\r'
+b":I19F0000\r": b":I1BF0000\r", # AZ  7 -> 6.5
+b":I29F0000\r": b":I2BF0000\r", # ALT 7 -> 6.5
+b":I16A0000\r": b":I19F0000\r", # AZ  8 -> 7
+b":I26A0000\r": b":I29F0000\r", # ALT 8 -> 7
+b":I1500000\r": b":I17A0000\r", # AZ  9 -> 7.5
+b":I2500000\r": b":I27A0000\r", # ALT 9 -> 7.5
+
+# GOTO SLEW SPEED (larger hex number -> slower)
 # replace them with slow long goto speed commands
 # main AZ  encoders loose counts with < 1C00000
 # main ALT encoders loose counts with < 1600000
@@ -73,13 +84,15 @@ b"AT+GMR\r\n": b"",
 #b":W1050000\r": b":W1040000\r", # rewrite AZ  main as AZ  auxiliary encoder
 #b":W2050000\r"; b":W2040000\r", # rewrite ALT main as ALT auxiliary encoder
 
-# SLOW MANUAL SLEW
-#b":I1500000\r": b":I1600000\r", # AZ  8 -> 7.5
-#b":I2500000\r": b":I2600000\r", # ALT 8 -> 7.5
-#b":I1500000\r": b":I1600000\r", # AZ  9 -> 8
-#b":I2500000\r": b":I2600000\r", # ALT 9 -> 8
+# slow to allow correct counting, 5V operation and quiet
 
-# SLOW GOTO (larger number = slower)
+# MANUAL SLEW SPEED (larger hex number -> slower)
+b":I19F0A00\r": b":I1EE0C00\r", # AZ  8 -> 7.5
+b":I29F0A00\r": b":I2EE0C00\r", # ALT 8 -> 7.5
+b":I1F70700\r": b":I19F0A00\r", # AZ  9 -> 8
+b":I2F70700\r": b":I29F0A00\r", # ALT 9 -> 8
+
+# GOTO SLEW SPEED (larger hex number -> slower)
 # 0.12 RPM slow silent
 #b":M1AC0D00\r": b":T1002000\r", # AZ  brake -> AZ  goto speed
 #b":M2AC0D00\r": b":T2002000\r", # ALT brake -> ALT goto speed
